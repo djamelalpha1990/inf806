17aea1714fde1ccdea7a8747f6b4b996a36a1754
djamelalpha1990 <djamelalpha1990@gmail.com>
2026-03-24 19:09:57 -0400

    Update api.js

--- functions/api.js
+++ functions/api.js
@@ -1,9 +1,18 @@
 
-export async function onRequest() {
-  return new Response(JSON.stringify({
-    message: "Hello depuis Cloudflare Functions 🚀",
-    success: true
-  }), {
+export async function onRequest({ env }) {
+  // insérer un message
+  await env.DB.prepare(
+    "INSERT INTO messages (content, created_at) VALUES (?, ?)"
+  )
+  .bind("Hello depuis la base ✅", new Date().toISOString())
+  .run();
+
+  // lire les messages
+  const { results } = await env.DB
+    .prepare("SELECT * FROM messages ORDER BY id DESC")
+    .all();
+
+  return new Response(JSON.stringify(results, null, 2), {
     headers: { "Content-Type": "application/json" }
   });
 }
